c7404ebac3444f0499eb4ddb2a3459d75d42fdc4
Furkan Gurbuz <furkangurbuz5@gmail.com>
2025-12-19 20:32:50 +0100

    Add task and Task type

--- src/app/tasks/tasks.component.ts
+++ src/app/tasks/tasks.component.ts
@@ -1,12 +1,21 @@
 import {Component, Input} from '@angular/core';
+import { TaskComponent} from "./task/task.component";
 
 @Component({
-  selector: 'app-task',
+  selector: 'app-tasks',
   standalone: true,
-  imports: [],
+  imports: [TaskComponent],
   templateUrl: './tasks.component.html',
   styleUrl: './tasks.component.css'
 })
 export class TasksComponent {
   @Input() userName?: string;
+  tasks = [
+    {
+      id: 1,
+      title: 'Learn angular at a professional level',
+      summary: 'Learn the basic and advanced features of angular and how to apply them so that we can get a better job.',
+      dueDate: '2026-03-31'
+    }
+  ]
 }
