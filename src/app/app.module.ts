3ff20ef4b2a879db0faf2ce776ddf7c1109559ff
Furkan Gurbuz <furkangurbuz5@gmail.com>
2025-12-20 21:44:01 +0100

    Move import

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import {NgModule} from "@angular/core";
 import {BrowserModule} from "@angular/platform-browser";
+import {FormsModule} from "@angular/forms";
 
 import {AppComponent} from "./app.component";
 import {HeaderComponent} from "./header/header.component";
@@ -8,7 +9,6 @@ import {TasksComponent} from "./tasks/tasks.component";
 import {CardComponent} from "./shared/card/card.component";
 import {TaskComponent} from "./tasks/task/task.component";
 import {NewTaskComponent} from "./tasks/new-task/new-task.component";
-import {FormsModule} from "@angular/forms";
 
 @NgModule({
   imports: [
